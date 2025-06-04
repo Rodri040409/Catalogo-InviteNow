f8a9adeb3f72af4b4b7b7429ebaa04e853e4ae34
Rodri040409 <rodrigo.ruiz.aguilar.cbtis165@gmail.com>
2025-06-03 19:41:06 -0600

    Actualización de scroll

--- src/components/Secciones/Planes.tsx
+++ src/components/Secciones/Planes.tsx
@@ -214,22 +214,36 @@ export default function Planes({
 
       {/* Categorías de eventos para filtración */}
       {categoria && (
-        <div className="w-full px-4 mt-8 mb-[-8rem] relative z-20 overflow-y-hidden lg:overflow-visible">
-          <div className="overflow-visible">
-            <div className="max-w-[1200px] mx-auto overflow-visible">
-              <div className="relative lg:overflow-visible overflow-x-auto overflow-y-hidden scrollbar-none">
-                <div className="flex justify-center lg:justify-end min-w-max overflow-visible">
-                  <GooeyNav
-                    items={eventosItems}
-                    initialActiveIndex={activeEventoIndex}
-                    onChange={onSelectEvento}
-                    particleCount={15}
-                    particleDistances={[90, 10]}
-                    particleR={100}
-                    animationTime={600}
-                    timeVariance={300}
-                    colors={[1, 2, 3, 1, 2, 3, 1, 4]}
-                  />
+        <div className="relative">
+          <div className="absolute right-2 bottom-0 z-30 animate-bounce lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5 text-white opacity-70"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
+            </svg>
+          </div>
+          {/* Aquí el contenedor actual de GooeyNav */}
+          <div className="w-full px-4 mt-8 mb-[-8rem] relative z-20 overflow-y-hidden lg:overflow-visible">
+            <div className="overflow-visible">
+              <div className="max-w-[1200px] mx-auto overflow-visible">
+                <div className="relative lg:overflow-visible overflow-x-auto overflow-y-hidden scrollbar-none">
+                  <div className="flex justify-center lg:justify-end min-w-max overflow-visible">
+                    <GooeyNav
+                      items={eventosItems}
+                      initialActiveIndex={activeEventoIndex}
+                      onChange={onSelectEvento}
+                      particleCount={15}
+                      particleDistances={[90, 10]}
+                      particleR={100}
+                      animationTime={600}
+                      timeVariance={300}
+                      colors={[1, 2, 3, 1, 2, 3, 1, 4]}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
